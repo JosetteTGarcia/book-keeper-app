bdb633ea28a0d067b5fbf8153fdc81ae75abb550
Josette Garcia <josettetgarcia@gmail.com>
2022-04-14 00:18:39 -0500

    Added link to Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/static/Home'
+import Signup from './components/sessions/Signup'
+import Login from './components/sessions/Login'
+import BookList from './components/books/BookList'
+import BookForm from './components/books/BookForm'
+import NavBar from './components/navigation/NavBar'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/books" element={<BookList />} />
+          <Route path="/books/new" element={<BookForm />} />
+        </Routes>
+      </Router>
+    
   );
 }
 
--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar(){
   
   
   return (
     <div>
-      NavBar
+     <ul> 
+       <li><Link to="/"> Home </Link></li>
+       <li><Link to="/signup"> Create Account </Link></li>
+       <li><Link to="/login"> Login </Link></li>
+       <li><Link to="/books"> All Books </Link></li>
+       <li><Link to="/books/new"> Add New Book </Link></li>
+       <li>Logout</li>
+     </ul>
     </div>
   )
 
